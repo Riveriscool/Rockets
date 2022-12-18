50193cd8eafda5f8d8d08eead3a53a594d77beae
Riveriscool <117595886+Riveriscool@users.noreply.github.com>
2022-12-18 16:16:08 -0500

    Add files via upload

--- rockets/ballistic_reentry.py
+++ rockets/ballistic_reentry.py
@@ -1,9 +1,9 @@
 #ballistic reentry in python, assuming lift is zero
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as I
-
-ha = np.linspace(0,122000,100000)#100km
+import scipy.integrate as sci
+he = 122000.0
+ha = np.linspace(0,he,100000)#100km
 Re = 6378000
 Ve = 7500.0
 beta = 0.1354/1000.0#density constant
@@ -18,16 +18,59 @@ dhdt = drdt
 dVdh = (Va[0:-2]-Va[1:-1]/(ha[0:-2]-ha[1:-1]))#find first order derivitive
 accela = dVdh*dhdt[0:-2]
 
+def Derivatives(t, state):
+    #state vector
+    V = state[0]
+    h = state[1]
+    gamma = state[2]
+    #Density model
+    rho = rhos*np.exp(-beta*h)
+    #Aerodynamic model
+    L = 0
+    
+    D = 0.5*rho*V**2*S*CD
+    #Gravity Model
+    gs = 9.81
+    g = gs*(R/(r+h))**2
+    r = R + h
+    
+    #Dynamics
+    dvdt = -D/m - g*np.sin(gamma)
+    dgammadt = (L/m - (g-V**2/r)*np.cos(gamma))/V
+    dhdt = V*np.sin(gamma)
+    
+    statedot = np.asarray([dvdt,dhdt,dgammadt])
+    
+    return statedot
+#integrate with odeint
+
+stateinitial = np.asarray([Ve,he,gammae])
+tout = np.linspace(0,100,1000)
+stateout = sci.odeint(Derivatives,stateinitial,tout)
+
+Vnum = stateout[:,0]
+hnum = stateout[:,1]
+gammanum = stateout[:,2]
+accelnum = (Vnum[0:-2]-Vnum[1:-1])/(tout[0:2]-tout[1:-1])
 
 plt.figure()
-plt.plot(ha,Va)
+plt.plot(ha,Va,label='Analytic')
+plt.plot(hnum,Vnum,label='Numerical')
 plt.xlabel('Altitude (m)')
 plt.ylabel('Velocity (m/s)')
 plt.grid()
 
 plt.figure()
-plt.plot(ha[0:-2],accela/9.81)
+plt.plot(ha[0:-2],accela/9.81,label='Analytic')
+plt.plot(hnum[0:-2],accelnum/9.81,label="Numerical")
 plt.xlabel('Time (sec)')
 plt.ylabel('Gs')
 plt.grid()
 plt.show()
+
+plt.figure()
+plt.plot(tout,gammanum)
+plt.ylabel('Flight path Angle (rad)')
+plt.xlabel('time')
+#Dear future River:
+#stop being a bitch and finish this
